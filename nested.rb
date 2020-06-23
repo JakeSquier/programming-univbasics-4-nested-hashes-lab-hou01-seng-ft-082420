2e531982d1b5ee20af9ab29874a507c3c5e901a7
Jacob Squier <jacob.squier@gmail.com>
2020-06-23 06:45:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,6 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+  return programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,15 +56,18 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  return programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
+# add the following information to the top level of
+  programmer_hash = {
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+  }
 # return the entire updated hash
 
 	programmer_hash = 
@@ -81,7 +85,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  return programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
@@ -103,7 +107,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +130,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  return programmer_hash
 end
